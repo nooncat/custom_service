106c59b2944fc85621d44d1f41716ea67f165fb0
Maxim Seliverstov <maxim.web.developer@gmail.com>
2018-10-15 00:41:31 +0400

    Fixes|

--- app/controllers/cabinet/temporary_storage_warehouse_transactions_controller.rb
+++ app/controllers/cabinet/temporary_storage_warehouse_transactions_controller.rb
@@ -7,7 +7,26 @@ class Cabinet::TemporaryStorageWarehouseTransactionsController < Cabinet::Applic
     @resource = TemporaryStorageWarehouseTransaction.new
   end
 
+  def create
+    @resource = TemporaryStorageWarehouseTransaction.new(permitted_params)
+
+    if @resource.save
+      redirect_to [:cabinet, :temporary_storage_warehouse_transactions], notice: 'Транспортное средство создано'
+    else
+      render :new
+    end
+  end
+
   def edit
     @resource = TemporaryStorageWarehouseTransaction.find(params[:id])
+  end
+
+  private
+
+  def permitted_params
+    params.require(:temporary_storage_warehouse_transaction).permit(
+      :company_id, :vehicle_number, :vehicle_type, :state, :pallet_count, :date,
+      :checkin_date, :checkin_notifie, :checkout_date, :checkout_notifie, :phone, :deal_typ, :country_code
+    )
   end
 end
